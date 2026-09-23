718c02c2513c9c235d47fd2b2b9518ba1241b861
Ubuntu <ubuntu@ip-172-31-1-72.eu-north-1.compute.internal>
2026-09-23 11:58:30 +0000

    docker assignment is running

--- DevOps/Docker_Sani_Rajesh_Lokhande/frontend/student_app.js
+++ DevOps/Docker_Sani_Rajesh_Lokhande/frontend/student_app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 const BACKEND_URL = process.env.BACKEND_URL;
+console.log(BACKEND_URL);
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
